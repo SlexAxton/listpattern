9eafb6c9bc93091bc374dae55a7e441a0f09dcc8
Alex Sexton <alexsexton@gmail.com>
2018-03-05 16:58:45 -0800

    Add logic for normal start, middle, end usage

--- index.js
+++ index.js
@@ -28,6 +28,72 @@ function extractListPatterns(data) {
   throw new Error('Could not find listPattern set in configuration data.');
 }
 
+// Logic to format the literal length rules (e.g. "2": "{0} and {1}")
+function formatLiteralPattern(list, listPattern) {
+  // The string we'll build up
+  var result = listPattern;
+
+  // This means we have a special handling for just this number of elements
+  list.forEach(function(listItem, idx) {
+    result = result.replace('{' + idx + '}', listItem);
+  });
+
+  return result;
+}
+
+function listPatternFormatter(list) {
+  var listPatternRules =
+    this.__cldr__[LIST_PATTERN_PREFIX + this.__config__.patternType] ||
+    this.__cldr__[LIST_PATTERN_PREFIX + DEFAULT_LIST_PATTERN];
+
+  if (!listPatternRules) {
+    throw new Error('Unable to find a valid list pattern or fallback pattern.');
+  }
+
+  // Check for literal length rules
+  if (listPatternRules.hasOwnProperty(list.length + '')) {
+    return formatLiteralPattern(list, listPatternRules[list.length + '']);
+  }
+
+  // If we're here, we've got a standard "start" "middle" "end" situation
+  var result = '';
+
+  // This is the "correct" output, but if someone got here, they probably
+  // already wanted to handle the empty case.
+  if (list.length < 1) {
+    return '';
+  }
+
+  // This is also a 'silent failure' mode, but ideally handled externally
+  if (list.length === 1) {
+    return list[0];
+  }
+
+  // Start with end
+  var pointer = list.length - 1;
+  result = listPatternRules.end
+    .replace('{1}', list[pointer])
+    .replace('{0}', list[pointer - 1]);
+
+  // We used up 2 indexes already
+  pointer = pointer - 2;
+
+  // Fill in the middle section
+  while (pointer > 0) {
+    result = listPatternRules.middle
+      .replace('{1}', result)
+      .replace('{0}', list[pointer]);
+    pointer -= 1;
+  }
+
+  // Handle the 'start' base-case
+  result = listPatternRules.start
+    .replace('{1}', result)
+    .replace('{0}', list[pointer]);
+
+  return result;
+}
+
 /**
  * @param {object} cldrData Ideally you just take the cldr-js value for your
  * locale and pass it straight in
@@ -38,19 +104,26 @@ function extractListPatterns(data) {
  * and default configuration rules
  */
 function ListPattern(cldrData, _defaultConfig) {
-  var formatter = function listPatternFormatter(list) {
-    return list.join(', ');
+  var formatter = listPatternFormatter;
+
+  var context = {
+    __cldr__: extractListPatterns(cldrData),
+    __config__: Object.assign(
+      {
+        patternType: DEFAULT_LIST_PATTERN
+      },
+      _defaultConfig || {}
+    )
   };
 
-  formatter.__cldr__ = extractListPatterns(cldrData);
-  formatter.__config__ = Object.assign(
-    {
-      patternType: DEFAULT_LIST_PATTERN
-    },
-    _defaultConfig || {}
-  );
+  // Return a bound function with the correct context
+  var result = formatter.bind(context);
 
-  return formatter;
+  // For tests sry…
+  result.__cldr__ = context.__cldr__;
+  result.__config__ = context.__config__;
+
+  return result;
 }
 
 module.exports = ListPattern;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,24 @@
 var ListPattern = require('./index');
 var MockConfig = require('./mock/config');
+var MockData = {
+  animals1: ['lions'],
+  animals2: ['lions', 'tigers'],
+  animals3: ['lions', 'tigers', 'bears'],
+  animals4: ['lions', 'tigers', 'bears', 'sharks'],
+  animals5: ['lions', 'tigers', 'bears', 'sharks', 'llamas'],
+  animals10: [
+    'lions',
+    'tigers',
+    'bears',
+    'sharks',
+    'llamas',
+    'zebras',
+    'chimps',
+    'hedgehogs',
+    'owls',
+    'wolves'
+  ]
+};
 
 describe('Setup', function() {
   test('Configuration object is stored', function() {
@@ -36,5 +55,48 @@ describe('Setup', function() {
     });
     expect(formatter.__config__).toHaveProperty('patternType');
     expect(formatter.__config__.patternType).toBe('or');
+  });
+});
+
+describe('Formatting', function() {
+  test('Formats a a list of 0 items', function() {
+    var formatter = ListPattern(MockConfig.test);
+    expect(formatter([])).toBe('');
+  });
+
+  test('Formats a a list of 1 item', function() {
+    var formatter = ListPattern(MockConfig.test);
+    expect(formatter(MockData.animals1)).toBe('lions');
+  });
+
+  test('Formats a list of exactly 2', function() {
+    var formatter = ListPattern(MockConfig.test);
+    expect(formatter(MockData.animals2)).toBe('lions and tigers');
+  });
+
+  test('Formats a list of 3', function() {
+    var formatter = ListPattern(MockConfig.test);
+    expect(formatter(MockData.animals3)).toBe('lions, tigers, and bears');
+  });
+
+  test('Formats a list of 4', function() {
+    var formatter = ListPattern(MockConfig.test);
+    expect(formatter(MockData.animals4)).toBe(
+      'lions, tigers, bears, and sharks'
+    );
+  });
+
+  test('Formats a list of 5', function() {
+    var formatter = ListPattern(MockConfig.test);
+    expect(formatter(MockData.animals5)).toBe(
+      'lions, tigers, bears, sharks, and llamas'
+    );
+  });
+
+  test('Formats a list of 10', function() {
+    var formatter = ListPattern(MockConfig.test);
+    expect(formatter(MockData.animals10)).toBe(
+      'lions, tigers, bears, sharks, llamas, zebras, chimps, hedgehogs, owls, and wolves'
+    );
   });
 });
